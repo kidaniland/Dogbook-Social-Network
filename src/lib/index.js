5484687486aca2cfdad2f2f43047eb6a70d5be97
kidaniland <mdcvillas@gmail.com>
2021-02-04 18:56:39 -0300

    Style y html de login completo

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,3 +4,21 @@ export const myFunction = () => {
   // aqui tu codigo
   console.log('Hola mundo!');
 };
+
+/*const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");*/
+
+
+// Initialize Cloud Firestore through Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyAaODj-xRqenmcztHx7NlQSdjn1qymLsw4",
+  authDomain: "social-network-17cd8.firebaseapp.com",
+  projectId: "social-network-17cd8",
+  storageBucket: "social-network-17cd8.appspot.com",
+  messagingSenderId: "1098491151135",
+  appId: "1:1098491151135:web:f45ebeff7f405f84c9262a",
+  measurementId: "G-RF5REDL25N"
+});
+
+var db = firebase.firestore();
--- src/main.js
+++ src/main.js
@@ -3,3 +3,23 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+const inputs = document.querySelectorAll('.input');
+
+function focusFunction(){
+    let parent = this.parentNode.parentNode;
+    parent.classList.add('focus');
+}
+
+function blurFunction(){
+    let parent = this.parentNode.parentNode;
+    if(this.value === ""){
+        parent.classList.remove('focus');
+    }
+}
+
+inputs.forEach(input => {
+    input.addEventListener('focus', focusFunction); 
+    input.addEventListener('blur', blurFunction); 
+
+});

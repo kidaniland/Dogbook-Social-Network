417622029900a2d1207458a148724574d40b1e24
kidaniland <mdcvillas@gmail.com>
2021-03-04 18:42:49 -0300

    Agregado el HTML de la vista footer y editado sidebar derecho de la vista wall

--- src/lib/view/footer-view.js
+++ src/lib/view/footer-view.js
@@ -1,7 +1,16 @@
 const footerView = `
     <footer>
-        <div id="footer--bar">
-            <h1>TODO ESTO ES EL FOOTER</h1>
+        <div class="wrapper">
+        <div class="foot">
+
+            <ul>
+                <li><a href="">Sobre Dogbook</a></li>
+                <li><a href="">Contacto</a></li>
+                <li><a href="">Política de privacidad</a></li>
+                <li><a href="">Ayuda</a></li>
+            </ul>
+
+            <p>Copyright ©mdcvillas 2021 Todos los derechos reservados.</p>
         </div>
     </footer>
 `;
--- src/lib/view/wall-view.js
+++ src/lib/view/wall-view.js
@@ -5,14 +5,100 @@ const wallView = () => {
     const wallContent = `
         ${headerView}
         <div class ="wall--content">
+
             <div class="perfil--resumen">
-                <h1>RESUMEN DEL PERFIL</h1>
+                <div class="sidebar--left">
+
+                    <img src="./image/avatar.png" class="img--side">
+                    <p class="text--side">
+                        Soy: </br> 
+                        <span class="span--side" id="sideUser">
+                            Firulais
+                        </span>
+                    </p>
+                    <p class="text--side">
+                        Mi human@ es: </br>
+                        <span class="span--side" id="sideName">
+                            Daniela Villa
+                        </span>
+                    </p>
+                    <p class="text--side" id="sideCountry">
+                        Chile
+                    </p>
+                    <a href="" class="link--side "id="alProfile">Visitar perfíl</a>
+
+                </div>
             </div>
-            <div class="post--resumen">
-                <h1>LOS POST AGREGADOS</h1>
+
+
+            <div class="post--content">
+                <div class="main mainpost" style="margin-bottom:20px; padding-bottom:10px;">	
+
+                    <div class="userimg">
+                        <img src="./image/avatar.png">
+                    </div>
+
+                    <div class="username">				 
+                        <p class="name" style="top:15px;">Firulais</p>
+                    </div>
+
+                    <p class="quotes">
+                        <textarea id="mypara" placeholder="¿Qué estás oliendo?, envía un imagen o ladrido"></textarea>
+                    </p>
+
+                    <!--botones de interaccion-->
+
+                    <div class="post">
+                        <img id="load2" class="postimg" src=" ">
+                    </div>
+
+                    <div class="postbar">
+                        <input type="file" accept="images/*" id="chooseimg">
+                        <button type="button" class="imgbttn" id="imgbttn">
+                            <i class="far fa-image"></i>
+                        </button>
+                        <span class="span--like" id="like-number">0</span>
+                        <button type="button" id="likepost" class="postmypost">
+                            <i class="fas fa-paw"></i>
+                        </button>
+                        <button type="button" id="postmypost" class="postmypost">
+                            <i class="far fa-comment"></i>
+                        </button>
+                    </div>
+
+                 </div>
             </div>
+
+
             <div class="recomendacion">
-                <h1>Recomendaciones o fotos de otros perfiles</h1>
+                <div class="sidebarright">
+
+                    <div class="right-content">
+                        <img src="./image/avatar-dog.png">
+                        <p class="name">Dog 1</p>
+                        <a href="" id="addFriend1">Agregar amigo</a>
+
+                    </div>
+
+                    <div class="right-content">
+                        <img src="./image/avatar-dog.png">
+                        <p class="name">Dog 2</p>
+                        <a href="" id="addFriend2">Agregar amigo</a>
+                    </div>
+
+                    <div class="right-content">
+                        <img src="./image/avatar-dog.png">
+                        <p class="name">Dog 3</p>
+                        <a href="" id="addFriend3">Agregar amigo</a>
+                    </div>
+
+                    <div class="right-content">
+                        <img src="./image/avatar-dog.png">
+                        <p class="name">Dog 4</p>
+                        <a href="" id="addFriend4">Agregar amigo</a>
+                    </div>
+
+                </div>    
             </div>
         </div> 
         ${footerView}
